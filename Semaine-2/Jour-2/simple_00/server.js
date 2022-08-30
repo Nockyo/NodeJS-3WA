a5eccdbbc6f89c6a7139606440e4f22b1c6f8634
Seniword <joey.pages8@gmail.com>
2022-08-30 12:34:55 +0200

    j2

--- Semaine-2/Jour-2/simple_00/server.js
+++ Semaine-2/Jour-2/simple_00/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import session from "express-session";
 import path from "path";
 import { fileURLToPath } from "url";
+import home from './routes/home.js'
 
 // ==========
 // App initialization
@@ -17,14 +19,44 @@ const app = express();
 // ==========
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+const checkOddEven = (req, res, next) =>{
+  if(req.params.number % 2 == 0)
+    req.message = "Le nombre est pair."
+  else
+    req.message = "Le nombre est impair."
+
+  next();
+}
+
+const incrementCount = (req, res, next) => {
+
+  if(req.session.count)
+    req.session.count++;
+
+  else
+    req.session.count = 1;
+
+  console.log(req.session.count);
+  next();
+}
 
 // ==========
 // App routes
 // ==========
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+app.get("/", incrementCount, home);
+
+app.get("/check/:number", checkOddEven, (req, res) =>{
+  const message = req.message
+  res.json({message})
+})
 
 // ==========
 // App start
